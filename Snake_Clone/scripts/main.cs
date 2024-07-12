945a6abfe7a8d3522e4249d518f82be9e2c6e933
Jaden Thakur <70930877+Jaden-Thakur@users.noreply.github.com>
2024-07-12 14:59:38 -0400

    added vars fixed error with declaring variables outside of class

--- Snake_Clone/scripts/main.cs
+++ Snake_Clone/scripts/main.cs
@@ -1,21 +1,50 @@
 using Godot;
 using System;
 
-//game vars
-//int score;
-//bool game_started = false;
 
-// game variables
-//int cells = 20;
-//int cell_size = 50;
+
+ 
+
 
 public partial class main : Node2D
 {
-	[Export]
-	public int Test = 1;
+		// game vars
+	int score= 0;
+	bool game_started = false;
+
+	// game variables
+	int cells = 20;
+	int cell_size = 50;
+	// snake vars
+	int[] old_data;
+	int[] snake_data;
+	PackedScene[] snake;
+
+	//movement vars
+	int[] start_pos = {9, 9};
+	int[] up = {0, -1};
+	int[] down = {0, 1};
+	int[] left = {-1, 0};
+	int[] right = {1, 0};
+	int[] move_direction;
+	bool can_move = true;
+	
+	public void new_game() {
+		score = 1;
+		String score_label = GetNode<Label>("Background/Hud/Score").Text;
+		score_label = "SCORE: " + $"{score}";
+		move_direction = up;
+		can_move = true;
+		//generate_snake();
+	} 
+
+	[Export] public PackedScene snake_scene;
 	// Called when the node enters the scene tree for the first time.
+	
+	
 	public override void _Ready()
 	{
+		new_game();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,3 +52,5 @@ public partial class main : Node2D
 	{
 	}
 }
+
+
