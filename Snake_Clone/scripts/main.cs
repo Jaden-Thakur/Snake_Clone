bb33d882067103c8ec981d2a89442e453dde3e98
Jaden Thakur <70930877+Jaden-Thakur@users.noreply.github.com>
2024-07-16 09:45:33 -0400

    idk

--- Snake_Clone/scripts/main.cs
+++ Snake_Clone/scripts/main.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-
+using System.Collections.Generic;
 
 
  
@@ -16,29 +16,49 @@ public partial class main : Node2D
 	int cells = 20;
 	int cell_size = 50;
 	// snake vars
-	int[] old_data;
-	int[] snake_data;
-	PackedScene[] snake;
+	List<Vector2> old_data = new List<Vector2>();
+	List<Vector2> snake_data  = new List<Vector2>();
+	List<Node> snake = new List<Node>();
 
 	//movement vars
-	int[] start_pos = {9, 9};
-	int[] up = {0, -1};
-	int[] down = {0, 1};
-	int[] left = {-1, 0};
-	int[] right = {1, 0};
-	int[] move_direction;
+	Vector2 start_pos = new Vector2(9, 9);
+	Vector2 up = new Vector2(0, -1);
+	Vector2 down = new Vector2(0, 1);
+	Vector2 left = new Vector2(-1, 0);
+	Vector2 right = new Vector2(1, 0);
+	Vector2 move_direction;
 	bool can_move = true;
 	
+	[Export] public PackedScene snake_scene;
+	
 	public void new_game() {
-		score = 1;
+		score = 0;
 		Label ScoreText = GetNode<Label>("Background/Hud/Score"); 
 		ScoreText.Text = "SCORE: " + $"{score}";
 		move_direction = up;
 		can_move = true;
-		//generate_snake();
+		generate_snake();
 	} 
+	
+	public void generate_snake() {
+		old_data.Clear();
+		snake_data.Clear();
+		snake.Clear();
+		
+		for(int i = 0; i < 3; i++) {
+			add_segment(start_pos + new Vector2(0,1));
+		}
+	}
+	
+	public void add_segment(Vector2 pos) {
+		snake_data.Add(pos);
+		Node SnakeSegment = snake_scene.Instantiate();
+		SnakeSegment.Position = (pos * cell_size) + new Vector2(0, cell_size);
+		AddChild(SnakeSegment);
+		snake.Add(SnakeSegment);
+	}
 
-	[Export] public PackedScene snake_scene;
+
 	// Called when the node enters the scene tree for the first time.
 	
 	
@@ -50,10 +70,8 @@ public partial class main : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-		float AMOUNT = 5;
 		if (Input.IsKeyPressed(Key.W)) {
-			GetNode<Label>("Background/Hud/Score").Text = "Test";
+			
 		}
 	}
 }
